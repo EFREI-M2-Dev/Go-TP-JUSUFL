959f290d528564511e8ed33f7ccb29576411b4e8
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-05-14 17:08:24 +0200

    feat(test): add test for addContact

--- src/main_internal_test.go
+++ src/main_internal_test.go
@@ -84,3 +84,60 @@ func TestDeleteContact(t *testing.T) {
 		t.Error("Le contact 'Alice' n'a pas été supprimé.")
 	}
 }
+
+func TestAddContact(t *testing.T) {
+	testData := PhoneBook{
+		Contacts: []Contact{
+			{Name: "Alice", Number: "1234"},
+			{Name: "Bob", Number: "5678"},
+		},
+	}
+
+	err := savePhonebook(testData)
+	if err != nil {
+		t.Fatalf("Erreur lors de la sauvegarde du phonebook : %v", err)
+	}
+
+	err = addContact("Charlie", "91011")
+	if err != nil {
+		t.Fatalf("Erreur lors de l'ajout du contact : %v", err)
+	}
+
+	phonebook, err := loadPhonebook()
+	if err != nil {
+		t.Fatalf("Erreur lors du chargement du phonebook après ajout : %v", err)
+	}
+
+	var contactFound bool
+	for _, c := range phonebook.Contacts {
+		if c.Name == "Charlie" && c.Number == "91011" {
+			contactFound = true
+		}
+	}
+
+	if !contactFound {
+		t.Error("Le contact 'Charlie' n'a pas été ajouté correctement.")
+	}
+
+	contactFound = false
+	for _, c := range phonebook.Contacts {
+		if c.Name == "Alice" && c.Number == "1234" {
+			contactFound = true
+		}
+	}
+
+	if !contactFound {
+		t.Errorf("Le contact 'Alice' a été supprimé.")
+	}
+
+	contactFound = false
+	for _, c := range phonebook.Contacts {
+		if c.Name == "Bob" && c.Number == "5678" {
+			contactFound = true
+		}
+	}
+
+	if !contactFound {
+		t.Errorf("Le contact 'Bob' a été supprimé.")
+	}
+}
