5bfc9a048cce59fe204f6d137e9bc0dce369adb4
Suylo <spoownz@yahoo.com>
2025-05-14 15:37:50 +0200

    refactor(main): simplify phonebook loading and improve action handling

--- src/main.go
+++ src/main.go
@@ -9,24 +9,23 @@ func main() {
 	action := flag.String("action", "", "Action to perform: add, list, update, delete")
 	flag.Parse()
 
-	annuaire, err := loadPhonebook()
+	_, err := loadPhonebook()
 	if err != nil {
 		fmt.Println("Error during loading :", err)
 		return
 	}
 
 	switch *action {
-		case "add":
-			fmt.Printf("add contact")
-		case "list":
-			for _, contact := range annuaire.Contacts {
-				fmt.Printf("%+v\n", contact)
-			}
-		case "update":
-			fmt.Printf("update contact")
-		case "delete":
-			fmt.Printf("delete contact")
-		default:
-			fmt.Println("Action inconnue")
+	case "add":
+		fmt.Printf("add contact")
+	case "list":
+		fmt.Printf("Liste des contacts :\n")
+		displayContacts()
+	case "update":
+		fmt.Printf("update contact")
+	case "delete":
+		fmt.Printf("delete contact")
+	default:
+		fmt.Println("Action inconnue")
 	}
 }
