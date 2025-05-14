cdaafcd4f3cf40a059c752a74b2071b843f1aca0
Florent Paris <florentparis28@gmail.com>
2025-05-14 15:18:24 +0200

    refactor(global): mv into src folder

--- go.mod
+++
@@ -1,3 +0,0 @@
-module annuaire
-
-go 1.24.3
--- src/main.go
+++ src/main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	listerContacts()
 }
+
